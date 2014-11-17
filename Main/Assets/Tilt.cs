1ab900d387296f01a7c539d5d5a99f7536e86c89
Landon Adams <Landona24@gmail.com>
2014-11-17 14:06:04 -0800

    Tilt Functionality
    Limitless platform tilting through euler angles

---
+++ Main/Assets/Tilt.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tilt : MonoBehaviour {
+	public int TiltSpeed = 16;
+	// Use this for initialization
+	void Start () {
+		//transform.Rotate (Vector3.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.rotation = Quaternion.identity;
+		if (transform.localEulerAngles.y != 0){
+			transform.eulerAngles=new Vector3(transform.eulerAngles.x,0,transform.eulerAngles.z);
+		}
+		//transform.rotation = Quaternion.identity;
+		if ((Input.GetKey ("up")) ){// & (transform.eulerAngles.x < 20) || (transform.eulerAngles.x > 340)){
+			transform.Rotate (Vector3.right, Time.deltaTime * TiltSpeed);
+		}
+		if ((Input.GetKey ("right")) ){// & (transform.eulerAngles.z < 20) || (transform.eulerAngles.z > 340)){
+			transform.Rotate (Vector3.back, Time.deltaTime * TiltSpeed);
+		}
+		if ((Input.GetKey ("down")) ){// & (transform.eulerAngles.x < 20) || (transform.eulerAngles.x > 340)){
+				transform.Rotate (Vector3.left, Time.deltaTime * TiltSpeed);
+		}
+		if ((Input.GetKey ("left")) ){// & (transform.eulerAngles.z < 20) || (transform.eulerAngles.z > 340)){
+			transform.Rotate (Vector3.forward, Time.deltaTime * TiltSpeed);
+		}
+		//if (Input.GetKey (KeyCode.None)){
+		//	transform.rotation = Quaternion.identity;
+		//}
+	}
+}
