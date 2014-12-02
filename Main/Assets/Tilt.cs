ec7af6d7dae47c62b62a34e47f05e1c852f5b02e
Landon Adams <Landona24@gmail.com>
2014-12-02 12:36:53 -0800

    Limited Platform Rotation
    30 - 330 degree rotation

--- Main/Assets/Tilt.cs
+++ Main/Assets/Tilt.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class Tilt : MonoBehaviour {
-	public int TiltSpeed = 16;
+	public int tiltSpeed = 32;
+	public int limitUpLeft = 20;
+	public int limitDownRight = 340;
+	public float RX;
+	public float RZ;
 	// Use this for initialization
 	void Start () {
 		//transform.Rotate (Vector3.
@@ -10,23 +14,60 @@ public class Tilt : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		RX = transform.localEulerAngles.x;
+		RZ = transform.localEulerAngles.z;
+
+		//if (transform.localEulerAngles.y != 0){
+		transform.eulerAngles=new Vector3(transform.eulerAngles.x,0,transform.eulerAngles.z);
+		//}
 		//transform.rotation = Quaternion.identity;
-		if (transform.localEulerAngles.y != 0){
-			transform.eulerAngles=new Vector3(transform.eulerAngles.x,0,transform.eulerAngles.z);
+//limiter
+		if (RX > limitUpLeft && RX < limitDownRight){
+			if (RX > limitUpLeft){
+				transform.eulerAngles=new Vector3(limitUpLeft,0,transform.eulerAngles.z);
+			}
+			if (RX < limitDownRight){
+				transform.eulerAngles=new Vector3(limitDownRight,0,transform.eulerAngles.z);
+			}
 		}
+//movement
+		else{ 
+		//if (RX <= limitUpLeft && RX >= limitDownRight){
+			if ((Input.GetKey ("up")) ){// & (transform.eulerAngles.x < limitUpLeft) || (transform.eulerAngles.x > limitDownRight)){
+				transform.Rotate (Vector3.right, Time.deltaTime * tiltSpeed);
+			}
+			if ((Input.GetKey ("down")) ){// & (transform.eulerAngles.x < limitUpLeft) || (transform.eulerAngles.x > limitDownRight)){
+				transform.Rotate (Vector3.left, Time.deltaTime * tiltSpeed);
+			}
+		}
+
+
+		transform.eulerAngles=new Vector3(transform.eulerAngles.x,0,transform.eulerAngles.z);
+
+//limiter
+		if (RZ > limitUpLeft && RZ < limitDownRight){
+			if (RZ > limitUpLeft){
+				transform.eulerAngles=new Vector3(transform.eulerAngles.x,0,limitUpLeft);
+			}
+			if (RZ < limitDownRight){
+				transform.eulerAngles=new Vector3(transform.eulerAngles.x,0,limitDownRight);
+			}
+		}
+//movement
+		else{
+		//if (RZ <= limitUpLeft && RZ >= limitDownRight){
+			if ((Input.GetKey ("right")) ){// & (transform.eulerAngles.z < limitUpLeft) || (transform.eulerAngles.z > limitDownRight)){
+				transform.Rotate (Vector3.back, Time.deltaTime * tiltSpeed);
+			}
+			
+			if ((Input.GetKey ("left")) ){// & (transform.eulerAngles.z < limitUpLeft) || (transform.eulerAngles.z > limitDownRight)){
+				transform.Rotate (Vector3.forward, Time.deltaTime * tiltSpeed);
+			}
+		}
+
 		//transform.rotation = Quaternion.identity;
-		if ((Input.GetKey ("up")) ){// & (transform.eulerAngles.x < 20) || (transform.eulerAngles.x > 340)){
-			transform.Rotate (Vector3.right, Time.deltaTime * TiltSpeed);
-		}
-		if ((Input.GetKey ("right")) ){// & (transform.eulerAngles.z < 20) || (transform.eulerAngles.z > 340)){
-			transform.Rotate (Vector3.back, Time.deltaTime * TiltSpeed);
-		}
-		if ((Input.GetKey ("down")) ){// & (transform.eulerAngles.x < 20) || (transform.eulerAngles.x > 340)){
-				transform.Rotate (Vector3.left, Time.deltaTime * TiltSpeed);
-		}
-		if ((Input.GetKey ("left")) ){// & (transform.eulerAngles.z < 20) || (transform.eulerAngles.z > 340)){
-			transform.Rotate (Vector3.forward, Time.deltaTime * TiltSpeed);
-		}
+
+
 		//if (Input.GetKey (KeyCode.None)){
 		//	transform.rotation = Quaternion.identity;
 		//}
---
+++ Main/Assets/Tiltv2.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Tiltv2 : MonoBehaviour {
+	public float TiltSpeed = 32;
+	public float limitUpLeft = 30;
+	public float limitDownRight = 330;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetKey ("home"))) {
+			while (transform.localEulerAngles.x != 0 && transform.localEulerAngles.z != 0){
+				transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.identity, TiltSpeed * Time.deltaTime);
+			}
+		}
+		else{
+			if ((Input.GetKey ("up"))){
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(limitUpLeft,0,transform.eulerAngles.z), TiltSpeed * Time.deltaTime);
+			}
+			if ((Input.GetKey ("down"))){
+				transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler(limitDownRight,0,transform.eulerAngles.z), TiltSpeed * Time.deltaTime);
+			}
+			if ((Input.GetKey ("left"))){	
+				transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.eulerAngles.x,0,limitUpLeft), TiltSpeed * Time.deltaTime);
+			}
+			if ((Input.GetKey ("right"))) {
+				transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler (transform.eulerAngles.x, 0, limitDownRight), TiltSpeed * Time.deltaTime);
+			}
+		}
+	}
+}
