0b804116bf1da6e0a93091103a9045425cd3a2bb
Landon Adams <Landona24@gmail.com>
2014-12-03 14:49:21 -0800

    Scaling tweaks
    Scale of 1 to .2

--- Main/Assets/Tiltv2.cs
+++ Main/Assets/Tiltv2.cs
@@ -14,21 +14,21 @@ public class Tiltv2 : MonoBehaviour {
 	void Update () {
 		if ((Input.GetKey ("home"))) {
 			while (transform.localEulerAngles.x != 0 && transform.localEulerAngles.z != 0){
-				transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.identity, TiltSpeed * Time.deltaTime);
+				transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.identity, TiltSpeed * Time.deltaTime);
 			}
 		}
 		else{
 			if ((Input.GetKey ("up"))){
-				transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(limitUpLeft,0,transform.eulerAngles.z), TiltSpeed * Time.deltaTime);
+				transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(limitUpLeft,0,transform.localEulerAngles.z), TiltSpeed * Time.deltaTime);
 			}
 			if ((Input.GetKey ("down"))){
-				transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler(limitDownRight,0,transform.eulerAngles.z), TiltSpeed * Time.deltaTime);
+				transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.Euler(limitDownRight,0,transform.localEulerAngles.z), TiltSpeed * Time.deltaTime);
 			}
 			if ((Input.GetKey ("left"))){	
-				transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(transform.eulerAngles.x,0,limitUpLeft), TiltSpeed * Time.deltaTime);
+				transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(transform.localEulerAngles.x,0,limitUpLeft), TiltSpeed * Time.deltaTime);
 			}
 			if ((Input.GetKey ("right"))) {
-				transform.rotation = Quaternion.RotateTowards (transform.rotation, Quaternion.Euler (transform.eulerAngles.x, 0, limitDownRight), TiltSpeed * Time.deltaTime);
+				transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.Euler (transform.localEulerAngles.x, 0, limitDownRight), TiltSpeed * Time.deltaTime);
 			}
 		}
 	}
