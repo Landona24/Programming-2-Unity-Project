20e55fc61f65e23afe49e711df6c1b7815a2f59f
Landon Adams <Landona24@gmail.com>
2014-12-08 11:58:17 -0800

    Proper Axis z rotation and limits
    correct direction of rotation and limits tweaks

--- Main/Assets/Tiltv2.cs
+++ Main/Assets/Tiltv2.cs
@@ -4,6 +4,8 @@ public class Tiltv2 : MonoBehaviour {
 	public float TiltSpeed = 32;
 	public float limitUpLeft = 30;
 	public float limitDownRight = 330;
+	public float AxisVertical = Input.GetAxis ("Vertical");
+	public float AxisHorizontal = Input.GetAxis ("Horizontal");
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +14,10 @@ public class Tiltv2 : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		AxisVertical = Input.GetAxis ("Vertical");
+		AxisHorizontal = Input.GetAxis ("Horizontal");
+
 		if ((Input.GetKey ("home"))) {
 			while (transform.localEulerAngles.x != 0 && transform.localEulerAngles.z != 0){
 				transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.identity, TiltSpeed * Time.deltaTime);
@@ -19,16 +25,16 @@ public class Tiltv2 : MonoBehaviour {
 		}
 		else{
 			if ((Input.GetAxis ("Vertical") > 0)){
-				transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.Euler(limitDownRight,180,transform.localEulerAngles.z), TiltSpeed * (Input.GetAxis ("Vertical")) * Time.deltaTime);
+				transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.Euler(limitDownRight,180,transform.localEulerAngles.z), (TiltSpeed * (Input.GetAxis ("Vertical"))) * Time.deltaTime);
 			}
 			if ((Input.GetAxis ("Vertical") < 0)){
-					transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(limitUpLeft,180,transform.localEulerAngles.z), TiltSpeed * -(Input.GetAxis ("Vertical")) * Time.deltaTime);
+					transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(limitUpLeft,180,transform.localEulerAngles.z), (TiltSpeed * -(Input.GetAxis ("Vertical"))) * Time.deltaTime);
 			}
 			if ((Input.GetAxis ("Horizontal") < 0)){
-						transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.Euler (transform.localEulerAngles.x,180, limitDownRight), TiltSpeed * (Input.GetAxis ("Horizontal")) * Time.deltaTime);
+						transform.localRotation = Quaternion.RotateTowards (transform.localRotation, Quaternion.Euler(transform.localEulerAngles.x,180, limitDownRight), (TiltSpeed * -(Input.GetAxis ("Horizontal"))) * Time.deltaTime);
 			}
 			if ((Input.GetAxis ("Horizontal") > 0)){	
-							transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(transform.localEulerAngles.x,180,limitUpLeft), TiltSpeed * -(Input.GetAxis ("Horizontal")) * Time.deltaTime);
+							transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.Euler(transform.localEulerAngles.x,180,limitUpLeft), (TiltSpeed * (Input.GetAxis ("Horizontal"))) * Time.deltaTime);
 			}
 		}
 	}
